12649ca888dacf126fa7144200da7223264e3d84
Meow0x7E <133293581+Meow0x7E@users.noreply.github.com>
2024-06-25 02:31:59 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::{
+    fmt::Display,
+    io::{self, Write},
+    thread,
+    time::Duration
+};
+
+/// 以慢速扫描的方式写入数据。
+///
+/// 该函数接收一个迭代器、一个时间间隔（`Duration`）和一个可写入的引用（`&mut W`），
+/// 并逐个从迭代器中取出元素，将它们转换为字符串并写入到提供的写入器（`writer`）中。
+/// 在每次写入后，都会刷新写入器并暂停指定的时间间隔（`dur`）。
+///
+/// # 参数
+///
+/// - `iter`: 要写入的元素的迭代器。
+/// - `dur`: 每次写入后暂停的时间间隔。
+/// - `writer`: 要写入的目标的可写引用。
+///
+/// # 类型参数
+///
+/// - `I`: 实现`IntoIterator`特质的类型，其元素可以转换为字符串。
+/// - `W`: 实现`Write`特质的类型，用于写入数据。
+///
+/// # 返回值
+///
+/// 如果写入过程中没有发生错误，则返回`Ok(())`；否则返回`Err(io::Error)`。
+///
+/// # 示例
+///
+/// ```rust
+/// use std::{io, thread, time, fs::File};
+///
+/// let mut stdout = io::stdout();
+///
+/// slow_scan_write("Hello World!".chars(), time::Duration::from_secs(1), &mut stdout).unwrap();
+/// ```
+///
+/// 在上面的示例中，我们将字符串"Hello World!"以每秒一个的速度写入到 stdout
+pub fn slow_scan_write<I, W>(iter: I, dur: Duration, writer: &mut W) -> Result<(), io::Error>
+where
+    I: IntoIterator,
+    I::Item: Display,
+    W: Write {
+    for item in iter {
+        writer.write_all(item.to_string().as_bytes())?;
+        writer.flush()?;
+        thread::sleep(dur);
+    }
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,97 @@
+use std::{
+    io::{stdin, BufRead, BufReader, Error},
+    sync::{
+        atomic::{AtomicBool, AtomicUsize, Ordering},
+        Arc
+    },
+    thread::{self, JoinHandle}
+};
+
+use clap::Parser;
+use console::Term;
+use signal_hook::{consts::signal, low_level::exit};
+use slow_scan_print::slow_scan_write;
+
+#[derive(Parser, Debug)]
+#[command(name = "slow-scan-print", version)]
+#[command(about = "Text is printed at fixed intervals by character or by line, and its name is inspired by SSTV (Slow Scan TV).")]
+#[command(long_about = "
+Text is printed at fixed intervals by character or by line, and its name is inspired by SSTV (Slow Scan TV).
+
+slow-scan-print Copyright (C) 2024 Meow0x7E <Meow0x7E@outlook.com>
+This program comes with ABSOLUTELY NO WARRANTY;
+This is free software, and you are welcome to redistribute it under certain conditions;
+")]
+#[command(author = "Meow0x7E <Meow0x7E@outlook.com>")]
+struct Args {
+    #[arg(short, long)]
+    #[arg(default_value_t = String::from("20ms"))]
+    #[arg(
+        help = "Set the time interval for text printing. Time units of nanoseconds (ns) and higher are supported. Multiple time values can be combined using the \"+\" symbol."
+    )]
+    time: String,
+
+    #[arg(short, long)]
+    #[arg(help = "Print text by character instead of line")]
+    line_mode: bool,
+
+    #[arg(short = 'c', long)]
+    #[arg(help = "Hides the cursor while printing and displays it when finished.")]
+    hide_cursor: bool
+}
+
+fn main() -> Result<(), Error> {
+    let args = Args::parse();
+    let time = duration_str::parse(args.time).expect("时间格式不正确!");
+    let mut term = Term::stdout();
+    let stdin = stdin();
+    let reader = BufReader::new(stdin.lock());
+
+    let done = Arc::new(AtomicBool::new(false));
+    let signal_hook = hook_sigint_signal(Arc::clone(&done));
+
+    if args.hide_cursor {
+        term.hide_cursor().unwrap()
+    }
+
+    if args.line_mode {
+        let lines = reader.lines().map(|line| line.unwrap() + "\n");
+        slow_scan_write(lines, time, &mut term)?;
+    } else {
+        for line in reader.lines() {
+            slow_scan_write(line.unwrap().chars(), time, &mut term)?;
+        }
+    }
+
+    if args.hide_cursor {
+        term.show_cursor().unwrap()
+    }
+
+    Arc::clone(&done).store(true, Ordering::Relaxed);
+    signal_hook.join().unwrap();
+    Ok(())
+}
+
+fn hook_sigint_signal(done: Arc<AtomicBool>) -> JoinHandle<()> {
+    thread::spawn(move || {
+        let term = Arc::new(AtomicUsize::new(0));
+        const SIGINT_U: usize = signal::SIGINT as usize;
+        signal_hook::flag::register_usize(signal::SIGINT, Arc::clone(&term), SIGINT_U).expect("注册 Signal Hook 失败");
+
+        loop {
+            match term.load(Ordering::Relaxed) {
+                0 => {
+                    if done.load(Ordering::Relaxed) {
+                        break;
+                    }
+                    continue;
+                }
+                SIGINT_U => {
+                    Term::stdout().show_cursor().unwrap();
+                    exit(1);
+                }
+                _ => unreachable!("The match should be unreachable.")
+            }
+        }
+    })
+}
