4e567de0216a5c2f526cbef91373c2d62f23d64c
Meow0x7E <133293581+Meow0x7E@users.noreply.github.com>
2025-02-01 21:51:50 +0800

    没有宏了，写宏会很痛苦
    添加了 i18n 支持，改了一点运行逻辑

--- src/lib.rs
+++ src/lib.rs
@@ -5,171 +5,26 @@ use std::{
     time::Duration
 };
 
-/// 以慢速扫描的方式写入数据。
-///
-/// 该函数接收一个迭代器、一个时间间隔（`Duration`）和一个可写入的引用（`&mut W`），
-/// 并逐个从迭代器中取出元素，将它们转换为字符串并写入到提供的写入器（`writer`）中。
-/// 在每次写入后，都会刷新写入器并暂停指定的时间间隔（`dur`）。
-///
-/// # 参数
-///
-/// - `iter`: 要写入的元素的迭代器。
-/// - `dur`: 每次写入后暂停的时间间隔。
-/// - `writer`: 要写入的目标的可写引用。
-///
-/// # 类型参数
-///
-/// - `I`: 实现`IntoIterator`特质的类型，其元素可以转换为字符串。
-/// - `W`: 实现`Write`特质的类型，用于写入数据。
-///
-/// # 返回值
-///
-/// 如果写入过程中没有发生错误，则返回`Ok(())`；否则返回`Err(io::Error)`。
-///
-/// # 示例
-///
-/// ```rust
-/// use std::{io, thread, time, fs::File};
-///
-/// let mut stdout = io::stdout();
-///
-/// slow_scan_write("Hello World!".chars(), time::Duration::from_secs(1), &mut stdout).unwrap();
-/// ```
-///
-/// 在上面的示例中，我们将字符串"Hello World!"以每秒一个的速度写入到 stdout
-pub fn slow_scan_write<I, W>(iter: I, dur: Duration, writer: &mut W) -> Result<(), io::Error>
-where
-    I: IntoIterator,
-    I::Item: Display,
-    W: Write {
-    for item in iter {
-        writer.write_all(item.to_string().as_bytes())?;
-        writer.flush()?;
-        thread::sleep(dur);
+pub trait SlowScanWrite {
+    fn slow_scan_write<I>(&mut self, iter: I, delay: Duration) -> Result<(), io::Error>
+    where
+        I: IntoIterator,
+        I::Item: Display;
+}
+
+impl<W: Write> SlowScanWrite for W {
+    fn slow_scan_write<I>(&mut self, iter: I, delay: Duration) -> Result<(), io::Error>
+    where
+        I: IntoIterator,
+        I::Item: Display
+    {
+        for item in iter {
+            self.write_all(item.to_string().as_bytes())?;
+            self.flush()?;
+            thread::sleep(delay);
+        }
+
+        // 返回成功，如果所有项都成功写入
+        Ok(())
     }
-    Ok(())
-}
-
-#[macro_export]
-macro_rules! print_chars {
-    ($display:expr) => {{
-        let dur = Duration::from_millis(20);
-        let mut stdout = io::stdout();
-        slow_scan_write(
-            $display.lines().flat_map(|l| {
-                let mut v: Vec<char> = format!("{}", l).chars().collect();
-                v.push('\n');
-                v
-            }),
-            dur,
-            &mut stdout
-        )
-    }};
-    ($display:expr, $dur:expr) => {{
-        let mut stdout = io::stdout();
-        slow_scan_write(
-            $display.lines().flat_map(|l| {
-                let mut v: Vec<char> = format!("{}", l).chars().collect();
-                v.push('\n');
-                v
-            }),
-            $dur,
-            &mut stdout
-        )
-    }};
-    ($display:expr, $dur:expr, $writer:expr) => {{
-        slow_scan_write(
-            $display.lines().flat_map(|l| {
-                let mut v: Vec<char> = format!("{}", l).chars().collect();
-                v.push('\n');
-                v
-            }),
-            $dur,
-            $writer
-        )
-    }};
-}
-
-#[macro_export]
-macro_rules! print_chars_with_reader {
-    ($reader:expr) => {{
-        let dur = Duration::from_millis(20);
-        let mut stdout = io::stdout();
-        slow_scan_write(
-            std::io::BufRead::lines($reader).flat_map(|l| {
-                let mut v: Vec<char> = format!("{}", l.unwrap_or(String::from(""))).chars(String::from("")).collect();
-                v.push('\n');
-                v
-            }),
-            dur,
-            &mut stdout
-        )
-    }};
-    ($reader:expr, $dur:expr) => {{
-        let mut stdout = io::stdout();
-        slow_scan_write(
-            std::io::BufRead::lines($reader).flat_map(|l| {
-                let mut v: Vec<char> = format!("{}", l.unwrap_or(String::from(""))).chars(String::from("")).collect();
-                v.push('\n');
-                v
-            }),
-            $dur,
-            &mut stdout
-        )
-    }};
-    ($reader:expr, $dur:expr, $writer:expr) => {{
-        slow_scan_write(
-            std::io::BufRead::lines($reader).flat_map(|l| {
-                let mut v: Vec<char> = format!("{}", l.unwrap_or(String::from(""))).chars().collect();
-                v.push('\n');
-                v
-            }),
-            $dur,
-            $writer
-        )
-    }};
-}
-
-#[macro_export]
-macro_rules! print_lines {
-    ($display:expr) => {{
-        let dur = Duration::from_millis(20);
-        let mut stdout = io::stdout();
-        slow_scan_write($display.lines().map(|l| format!("{}\n", l)), dur, &mut stdout)
-    }};
-    ($display:expr, $dur:expr) => {{
-        let mut stdout = io::stdout();
-        slow_scan_write($display.lines().map(|l| format!("{}\n", l)), $dur, &mut stdout)
-    }};
-    ($display:expr, $dur:expr, $writer:expr) => {{
-        slow_scan_write($display.lines().map(|l| format!("{}\n", l)), $dur, $writer)
-    }};
-}
-
-#[macro_export]
-macro_rules! print_lines_with_reader {
-    ($reader:expr) => {{
-        let dur = Duration::from_millis(20);
-        let mut stdout = io::stdout();
-        slow_scan_write(
-            std::io::BufRead::lines($reader).map(|l| format!("{}\n", l.unwrap_or(String::from("")))),
-            dur,
-            &mut stdout
-        )
-    }};
-    ($reader:expr, $dur:expr) => {{
-        let mut stdout = io::stdout();
-        slow_scan_write(
-            std::io::BufRead::lines($reader).map(|l| format!("{}\n", l.unwrap_or(String::from("")))),
-            $dur,
-            &mut stdout()
-        )
-    }};
-    ($reader:expr, $dur:expr, $writer:expr) => {{
-        slow_scan_write(
-            std::io::BufRead::lines($reader).map(|l| format!("{}\n", l.unwrap_or(String::from("")))),
-            $dur,
-            $writer
-        )
-    }};
 }
--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,136 @@
 use std::{
-    io::{self, stdin, BufReader},
+    io::{BufRead, BufReader, stdin},
     sync::{
-        atomic::{AtomicBool, AtomicUsize, Ordering},
-        Arc
+        Arc,
+        atomic::{AtomicUsize, Ordering}
     },
-    thread::{self, JoinHandle}
+    thread::{self},
+    time::Duration
 };
 
-use clap::Parser;
+use clap::{Arg, ArgAction, Command, arg};
 use console::Term;
+use rust_i18n::t;
 use signal_hook::{consts::signal, low_level::exit};
-use slow_scan_print::{print_chars_with_reader, print_lines_with_reader, slow_scan_write};
+use slow_scan_print::SlowScanWrite;
 
-#[derive(Parser, Debug)]
-#[command(name = "slow-scan-print", version)]
-#[command(about = "Text is printed at fixed intervals by character or by line, and its name is inspired by SSTV (Slow Scan TV).")]
-#[command(long_about = "
-Text is printed at fixed intervals by character or by line, and its name is inspired by SSTV (Slow Scan TV).
-
-slow-scan-print Copyright (C) 2024 Meow0x7E <Meow0x7E@outlook.com>
-This program comes with ABSOLUTELY NO WARRANTY;
-This is free software, and you are welcome to redistribute it under certain conditions;
-")]
-#[command(author = "Meow0x7E <Meow0x7E@outlook.com>")]
+#[derive(Debug)]
 struct Args {
-    #[arg(short, long)]
-    #[arg(default_value_t = String::from("20ms"))]
-    #[arg(
-        help = "Set the time interval for text printing. Time units of nanoseconds (ns) and higher are supported. Multiple time values can be combined using the \"+\" symbol."
-    )]
-    time: String,
-
-    #[arg(short, long)]
-    #[arg(help = "Print text by character instead of line")]
+    time: Duration,
     line_mode: bool,
-
-    #[arg(short = 'c', long)]
-    #[arg(help = "Hides the cursor while printing and displays it when finished.")]
     hide_cursor: bool
 }
 
-fn main() -> Result<(), io::Error> {
-    let args = Args::parse();
-    let time = duration_str::parse(args.time).expect("时间格式不正确!");
+rust_i18n::i18n!("locales", fallback = "zh-CN");
+
+fn main() {
+    rust_i18n::set_locale(&sys_locale::get_locale().unwrap_or_else(|| String::from("zh-CN")));
+
+    let args = parser_args();
+    let print_handle = thread::spawn(move || slow_scan_print(args));
+
+    let listen = Arc::new(AtomicUsize::new(0));
+    const SIGINT_U: usize = signal::SIGINT as usize;
+    signal_hook::flag::register_usize(signal::SIGINT, Arc::clone(&listen), SIGINT_U)
+        .unwrap_or_else(|_| panic!("{}", t!("error.c5b72dec-ae1b-49fb-8fb6-eff11313faea")));
+
+    let delay = duration_str::parse("100ms").unwrap_or_else(|_| unreachable!("{}", t!("error.dbf620f1-e275-44c3-929a-5a946ca5daae")));
+    loop {
+        match listen.load(Ordering::Relaxed) {
+            0 => {
+                if print_handle.is_finished() {
+                    break;
+                }
+                thread::sleep(delay);
+                continue;
+            }
+            SIGINT_U => {
+                break;
+            }
+            _ => {
+                unreachable!("{}", t!("error.14837326-ccb0-42cd-9b30-27a4e64b5d01"))
+            }
+        }
+    }
+
+    Term::stdout().show_cursor().unwrap();
+    exit(0);
+}
+
+fn parser_args() -> Args {
+    #[inline]
+    fn delay(it: &str) -> Duration {
+        match duration_str::parse_std(it) {
+            Ok(it) => it,
+            Err(_) => panic!("{}", t!("clap.delay.value_parser.err_msg"))
+        }
+    }
+
+    let matches = Command::new("slow-scan-print")
+        .about(t!("clap.about").to_string())
+        .author("Meow0x7E <Meow0x7E@outlook.com>")
+        .args(&[
+            arg!(delay: -d --delay <TIME>)
+                .short_alias('t')
+                .alias("time")
+                .action(ArgAction::Set)
+                .default_value("20ms")
+                .help(format!("{}", t!("clap.delay.help"))),
+            Arg::new("line-mode")
+                .short('l')
+                .long("line-mode")
+                .action(ArgAction::SetTrue)
+                .help(format!("{}", t!("clap.line-mode.help"))),
+            Arg::new("hide-cursor")
+                .short('c')
+                .long("hide-cursor")
+                .action(ArgAction::SetTrue)
+                .help(format!("{}", t!("clap.hide-cursor.help")))
+        ])
+        .get_matches();
+
+    Args {
+        time: match matches.get_one::<String>("delay") {
+            Some(it) => delay(it),
+            None => duration_str::parse_std("20ms").unwrap_or_else(|_| unreachable!("{}", t!("error.dbf620f1-e275-44c3-929a-5a946ca5daae")))
+        },
+        line_mode: match matches.get_one::<bool>("line-mode") {
+            Some(it) => *it,
+            None => false
+        },
+        hide_cursor: match matches.get_one::<bool>("hide-cursor") {
+            Some(it) => *it,
+            None => false
+        }
+    }
+}
+
+fn slow_scan_print(args: Args) {
     let mut term = Term::stdout();
     let stdin = stdin();
     let reader = BufReader::new(stdin.lock());
-
-    let done = Arc::new(AtomicBool::new(false));
-    let signal_hook = hook_sigint_signal(Arc::clone(&done));
 
     if args.hide_cursor {
         term.hide_cursor().unwrap()
     }
 
     if args.line_mode {
-        print_lines_with_reader!(reader, time, &mut term)?;
+        let iter = reader
+            .lines()
+            .map(|l| format!("{}\n", l.unwrap_or(String::from(""))));
+        term.slow_scan_write(iter, args.time)
+            .unwrap_or_else(|it| panic!("{}\n{}", t!("error.026d13a1-ea8b-409a-8a4c-2ba551b475db"), it));
     } else {
-        print_chars_with_reader!(reader, time, &mut term)?;
+        let iter = reader.lines().flat_map(|l| {
+            let mut v: Vec<char> = l.unwrap_or(String::from("")).chars().collect();
+            v.push('\n');
+            v
+        });
+        term.slow_scan_write(iter, args.time)
+            .unwrap_or_else(|it| panic!("{}\n{}", t!("error.026d13a1-ea8b-409a-8a4c-2ba551b475db"), it));
     }
 
     if args.hide_cursor {
         term.show_cursor().unwrap()
     }
-
-    Arc::clone(&done).store(true, Ordering::Relaxed);
-    signal_hook.join().unwrap();
-    Ok(())
-}
-
-fn hook_sigint_signal(done: Arc<AtomicBool>) -> JoinHandle<()> {
-    thread::spawn(move || {
-        let term = Arc::new(AtomicUsize::new(0));
-        const SIGINT_U: usize = signal::SIGINT as usize;
-        signal_hook::flag::register_usize(signal::SIGINT, Arc::clone(&term), SIGINT_U).expect("注册 Signal Hook 失败");
-
-        loop {
-            match term.load(Ordering::Relaxed) {
-                0 => {
-                    if done.load(Ordering::Relaxed) {
-                        break;
-                    }
-                    continue;
-                }
-                SIGINT_U => {
-                    Term::stdout().show_cursor().unwrap();
-                    exit(1);
-                }
-                _ => unreachable!("The match should be unreachable.")
-            }
-        }
-    })
 }
